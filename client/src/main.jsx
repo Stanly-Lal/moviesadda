95e42d2535875009f6731b01b0efd8a8ca9c2a50
Stanly Lal <stanlylal17@gmail.com>
2026-09-15 18:08:31 +0530

    fix PWA install banner first load

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,3 @@
-// import React, { StrictMode } from "react";
-
-// import { createRoot } from "react-dom/client";
-
-// import { BrowserRouter } from "react-router-dom";
-
-// import App from "./App";
-
-// import "./styles/global.css";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>,
-// );
-
-
 import React, { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
@@ -29,10 +10,8 @@ import "./styles/global.css";
 
 import { initializeInstallPrompt } from "./utils/installPrompt";
 
-
 // Initialize the PWA install prompt BEFORE React starts.
 initializeInstallPrompt();
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
